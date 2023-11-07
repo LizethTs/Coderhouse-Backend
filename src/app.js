79279c047bf1457c6fb85491c903071f7ba53d23
Lizeth Tancara <liz@MacBook-Air-de-Lizeth.local>
2023-11-07 16:42:19 -0300

    Clase 04, desafío 03, agregando modificaciones al servidor con express

--- src/app.js
+++ src/app.js
@@ -10,21 +10,22 @@ const products = new productsManager("./src/archivos/productos.json");
 
 //
 app.get("/", (req, res) => {
-  res.send("PRODUCTOS DISPONIBLES");
+  res.status(200).send("PRODUCTOS DISPONIBLES");
 });
 
 //Muestra todo los productos y recibe query de límite
 app.get("/products", async (req, res) => {
   try {
     let resultado = await products.getProducts();
-    //console.log("productos: ", resultado)
-
-    if (req.query.limit) {
+    console.log("productos: ", resultado)
+    let limits = req.query.limit;
+    if (limits && limits > 0) {
       resultado = resultado.slice(0, req.query.limit);
+      res.status(200).json({resultado})
     }
 
     res.setHeader("Content-Type", "application/json");
-    res.send({ filtros: req.query, resultado });
+    res.status(200).json({ filtros: req.query, resultado });
   } catch (error) {
     console.log("no hay productos");
   }
@@ -35,13 +36,10 @@ app.get("/products/:id", async (req, res) => {
   
   let id = req.params.id;
   id = parseInt(id);
-  if (isNaN(id)) {
-    return res.send("ERROR, por favor, ingrese un id númerico");
-  } else if (id >= 11) {
-    return res.send("No existe es PRODUCTO");
-  }
-
+  if(isNaN(id)) return res.status(400).json({error:" Por favor, ingrese un id númerico"});
+ 
   resultado = products.getProducts(id).find((product) => product.id === id);
+  if(!resultado) return res.status(404).json({error:`No se encontró el Id: ${id}`});
 
   res.setHeader("Content-Type", "application/json");
   res.status(200).json({ resultado });
